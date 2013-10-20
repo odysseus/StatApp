28605e1eabca171c14375a48a9efc10192349800
Ryan Case <rmcase@gmail.com>
2013-10-20 16:46:51 -0600

    Initial Commit

---
+++ TankCommander/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  TankCommander
+//
+//  Created by Ryan Case on 10/20/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TankCommander/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TankCommander
+//
+//  Created by Ryan Case on 10/20/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
